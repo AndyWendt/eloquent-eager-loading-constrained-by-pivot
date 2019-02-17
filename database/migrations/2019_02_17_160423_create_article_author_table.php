739d7239d4408c71033d60cac7b12e5ded66d8ea
Andy Wendt <andy@awendt.com>
2019-02-17 09:22:20 -0700

    Add migrations

---
+++ database/migrations/2019_02_17_160423_create_article_author_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleAuthorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_author', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('article_id')->index();
+            $table->unsignedInteger('author_id')->index();
+            $table->boolean("display")->index();
+            $table->timestamps();
+
+            $table->foreign('article_id')->references('id')->on('articles');
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_author');
+    }
+}
